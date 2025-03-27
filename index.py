f08229894a90bb102c280cfcc1a7bc09fe42367a
wuuxxn <owen20050723@gmail.com>
2025-03-27 10:12:40 +0800

    v2

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def index():
     homepage = "<h1>吳昕恩Python網頁(時間+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=wuuxxn&work=pu>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
     homepage += "<a href=/about>昕恩簡介網頁</a><br>"
     return homepage
@@ -33,6 +33,7 @@ def me():
 @app.route("/welcome", methods=["GET"])
 def welcome():
     user = request.values.get("nick")
+    w = request.values.get("work")
     return render_template("welcome.html", name= user)
 
 
