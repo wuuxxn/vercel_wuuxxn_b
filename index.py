920170612958b8253fcc4d4f5995b374f0a59555
wuuxxn <owen20050723@gmail.com>
2025-03-27 10:16:19 +0800

    v2

--- index.py
+++ index.py
@@ -34,7 +34,7 @@ def me():
 def welcome():
     user = request.values.get("nick")
     w = request.values.get("work")
-    return render_template("welcome.html", name= user)
+    return render_template("welcome.html", name= user,work=w)
 
 
 @app.route("/account", methods=["GET", "POST"])
